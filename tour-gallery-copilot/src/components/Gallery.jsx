34bec46a7917bf8cca565100c4c8f0606f80722c
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-04-23 02:10:47 -0400

    Implement Gallery component that displays list of tours

--- tour-gallery-copilot/src/components/Gallery.jsx
+++ tour-gallery-copilot/src/components/Gallery.jsx
@@ -1,14 +1,15 @@
 import TourCard from './TourCard'
 
 function Gallery({ tours, setTours }) {
+  // Function to remove a tour by its ID
   const removeTour = (id) => {
-    setTours(tours.filter((tour) => tour.id !== id))
+    setTours(tours.filter((tour) => tour.id !== id)) // Filter out the tour with the given ID
   }
 
   return (
     <div className="gallery">
       {tours.map((tour) => (
-        <TourCard key={tour.id} tour={tour} removeTour={removeTour} />
+        <TourCard key={tour.id} tour={tour} removeTour={removeTour} /> /* Render a TourCard for each tour */
       ))}
     </div>
   )
